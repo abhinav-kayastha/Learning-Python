d60f36f5efa8237a85e04fc7b2d11e718b0bf80b
abhin <abhinavk@metropolia.fi>
2022-08-22 17:37:03 +0300

    making of 3.py

---
+++ 2. Variables and interactive programs/3.py
@@ -0,0 +1,7 @@
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+
+perimeter = 2 * length + 2 * width
+area = length * width
+
+print("")
