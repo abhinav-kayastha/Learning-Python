035b31a63e0f0e579655eb4fedd30c676c61f5c6
abhin <abhinavk@metropolia.fi>
2022-08-22 17:39:26 +0300

    making of 4.py

--- 2. Variables and interactive programs/3.py
+++ 2. Variables and interactive programs/3.py
@@ -4,4 +4,5 @@ width = int(input("Enter the width of the rectangle: "))
 perimeter = 2 * length + 2 * width
 area = length * width
 
-print("")
+print("The perimeter of the rectangle is " + str(perimeter))
+print("The area of the rectangle is " + str(area))
