e0565d9d5dd706f19d6b2100957dace521657434
abhin <abhinavk@metropolia.fi>
2022-08-22 19:35:42 +0300

    cleaning up code of 5.py

--- 2. Variables and interactive programs/5.py
+++ 2. Variables and interactive programs/5.py
@@ -4,4 +4,4 @@ lots = float(input("Enter lots: "))
 
 inGrams = (talent * 20 * 32 * 13.3) + (pounds * 32 * 13.3) + (lots * 13.3)
 
-print("The weight in modern units: " + str(int(inGrams//1000)) + " kilograms" + " and " + str(inGrams % 1000) + " grams.")
+print("The weight in modern units: " + str(int(inGrams // 1000)) + " kilograms" + " and " + str(inGrams % 1000) + " grams.")
