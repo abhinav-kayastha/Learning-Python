3890d30adcef2aa5a404ff4dd9ea93c3b9884cb3
abhin <abhinavk@metropolia.fi>
2022-08-26 12:27:21 +0300

    fixed bug for 6.py: instead of random.gauss(), use random.uniform()

--- 4. While loops (while)/6.py
+++ 4. While loops (while)/6.py
@@ -4,14 +4,12 @@ numbersOfPoints = 1000000
 pointsInsideCircle = 0
 
 for x in range(numbersOfPoints):
-    pointX = random.gauss(-1, 1)
-    pointY = random.gauss(-1, 1)
-
+    pointX = random.uniform(-1, 1)
+    pointY = random.uniform(-1, 1)
     if pointX ** 2 + pointY ** 2 < 1:
         pointsInsideCircle = pointsInsideCircle + 1
     else:
         continue
 
-print(pointsInsideCircle)
 piApproximation = (4 * pointsInsideCircle)/numbersOfPoints
 print(f"Approximation of pi is {piApproximation}")
