920e8dfb329f8f936a52b26e2cfd4291cf70a305
abhin <abhinavk@metropolia.fi>
2022-08-29 14:31:20 +0300

    changing int() to float() to accomodate more values

--- 2. Variables and interactive programs/2.py
+++ 2. Variables and interactive programs/2.py
@@ -1,3 +1,3 @@
-radius = int(input("Enter the radius of a circle: "))
+radius = float(input("Enter the radius of a circle: "))
 area = 3.14 * radius**2
 print("The area of the circle is: " + str(area))
--- 2. Variables and interactive programs/3.py
+++ 2. Variables and interactive programs/3.py
@@ -1,5 +1,5 @@
-length = int(input("Enter the length of the rectangle: "))
-width = int(input("Enter the width of the rectangle: "))
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
 
 perimeter = 2 * length + 2 * width
 area = length * width
