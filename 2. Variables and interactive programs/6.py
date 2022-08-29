e621d34cc369d32e4ad1a060247060c90f0f391c
abhin <abhinavk@metropolia.fi>
2022-08-29 14:42:43 +0300

    making code cleaner

--- 2. Variables and interactive programs/6.py
+++ 2. Variables and interactive programs/6.py
@@ -3,14 +3,14 @@ from random import randint
 ThreeDigitCode = ""
 
 for x in range(3):
-    ThreeDigitCode = ThreeDigitCode + str(randint(0,9))
+    ThreeDigitCode = ThreeDigitCode + str(randint(0, 9))
 
 print(ThreeDigitCode)
 
 FourDigitCode = ""
 
 for y in range(4):
-    FourDigitCode = FourDigitCode + str(randint(1,6))
+    FourDigitCode = FourDigitCode + str(randint(1, 6))
 
 print(FourDigitCode)
 
