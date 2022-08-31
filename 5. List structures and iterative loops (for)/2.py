f76a0db7e4423d9243e3c4d8ba4a463a2fe1e5b4
abhin <abhinavk@metropolia.fi>
2022-08-31 10:44:23 +0300

    creation and finishing of 2.py, bug remaining: on the 24th line, the sort method and the argument reverse=True, yields None when printing.

---
+++ 5. List structures and iterative loops (for)/2.py
@@ -0,0 +1,24 @@
+NumbersList = []
+FiveGreatestNumbers = []
+
+# The code block below asks the user for a number and then appends it to NumbersList, if the user enters an empty
+# string, the program will stop taking numbers from the user, and then stops.
+
+while True:
+    UserInput = input("Enter a number: ")
+    if UserInput == "":
+        break
+    else:
+        NumbersList.append(float(UserInput))
+
+# The code block below checks the largest number in NumbersList 5 times, and in each iteration it appends the largest
+# number to FiveGreatestNumbers and removes that number from NumbersList for the next iteration to find the largest
+# number.
+
+for x in range(5):
+    LargestNumber = max(NumbersList)
+    FiveGreatestNumbers.append(LargestNumber)
+    NumbersList.remove(LargestNumber)
+
+
+print(FiveGreatestNumbers)
