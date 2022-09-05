04640034938797c852379bbcfc562486e9a6f6af
abhin <abhinavk@metropolia.fi>
2022-09-05 14:19:15 +0300

    finished 1.py

---
+++ 6. Functions/1.py
@@ -0,0 +1,14 @@
+import random
+
+
+def randomRoll():
+    return random.randint(1, 6)
+
+
+def main():
+    roll = None
+    while roll != 6:
+        roll = randomRoll()
+        print(roll)
+
+main()
