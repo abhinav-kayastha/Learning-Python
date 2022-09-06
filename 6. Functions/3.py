bc16b29b451b9585b21d52a9b0a1c3acf217fc7e
abhin <abhinavk@metropolia.fi>
2022-09-06 18:33:32 +0300

    creation of 3.py

--- 6. Functions/3.py
+++
@@ -1,8 +0,0 @@
-def gallonsToLiters(userVolume):
-    return userVolume / 3.785
-
-
-def main(userVolume):
-    print(gallonsToLiters(userVolume))
-
-main(userVolume = float(input("Enter your volume of gasoline in gallons: ")))
