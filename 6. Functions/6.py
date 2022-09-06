1d653b4eca5cf32e1766c7ef98945600fc01954c
abhin <abhinavk@metropolia.fi>
2022-09-06 19:41:41 +0300

    creation of 6.py

---
+++ 6. Functions/6.py
@@ -0,0 +1,20 @@
+import math
+
+def pizza_unit_price_calc(diameter, price):
+    radius = (diameter / 2) / 100
+    area = math.pi * radius ** 2
+    unit_price = price / area
+    return unit_price
+
+pizza_one_diameter = float(input("Enter the diameter of your pizza (in cm): "))
+pizza_one_price = float(input("Enter the price of your pizza (in euros): "))
+pizza_one_unit_price = pizza_unit_price_calc(pizza_one_diameter, pizza_one_price)
+
+pizza_two_diameter = float(input("Enter the diameter of your pizza (in cm): "))
+pizza_two_price = float(input("Enter the price of your pizza (in euros): "))
+pizza_two_unit_price = pizza_unit_price_calc(pizza_two_diameter, pizza_two_price)
+
+if pizza_one_unit_price > pizza_two_unit_price:
+    print("Get the first pizza.")
+else:
+    print("Get the second pizza.")
