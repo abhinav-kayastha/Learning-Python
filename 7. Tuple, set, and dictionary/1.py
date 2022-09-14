8ce0046afd6542ce848f9182dcdc1df4d3c77db3
abhin <abhinavk@metropolia.fi>
2022-09-14 09:26:48 +0300

    1.py bug fix: accidentally using the indexing the userMonth variable instead of seasons tuple.

--- 7. Tuple, set, and dictionary/1.py
+++ 7. Tuple, set, and dictionary/1.py
@@ -2,9 +2,9 @@ seasons = ("spring", "summer", "autumn", "winter")
 userMonth = int(input("Enter a month (1-12): "))
 
 if userMonth == 12 or userMonth == 1 or userMonth == 2:
-    print(f"The season at that time is {userMonth[1]}.")
+    print(f"The season at that time is {seasons[1]}.")
 elif userMonth == 3 or userMonth == 4 or userMonth == 5:
-    print(f"The season at that time is {userMonth[0]}.")
+    print(f"The season at that time is {seasons[0]}.")
 elif userMonth == 6 or userMonth == 7 or userMonth == 8:
     print(f"The season at that time is {seasons[1]}.")
 elif userMonth == 9 or userMonth == 10 or userMonth == 11:
