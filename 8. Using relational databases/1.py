a2f2301c6b461dff3c130b96de4a98ea478627d2
abhin <abhinavk@metropolia.fi>
2022-09-16 15:21:26 +0300

    finished 1.py, should technically work but multiple errors

---
+++ 8. Using relational databases/1.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+
+def getAirportNameAndTown(gps_code):
+    sql = "SELECT name, municipality FROM airport"
+    sql += " WHERE gps_code = " + gps_code + ";"
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            print(f"Name: {result[0][0]}, Town: {result[0][1]}")
+        return
+
+
+connection = mysql.connector.connect(
+    host="127.0.0.1",
+    port="3306",
+    database="flight_game",
+    user="root",
+    password="2012004",
+    autocommit=True
+
+)
+
+gps_code_from_user = input("Enter GPS Code: ")
+getAirportNameAndTown(gps_code_from_user)
